f1ae5fb34ea21a325211e41fce328c9d541dc469
Emmanuel Garzon <eggarzon@gmail.com>
2022-06-10 16:29:56 -0700

    primary game board initiated, work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import GameBoard from "./components/GameBoard";
 
-function App() {
+const App = () => {
   return (
     <div className="main-wrapper">
       <GameBoard />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function GameBoard() {
-  return <div>This is the gameboard.</div>;
-}
+const width = 8;
+const gameTiles = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const GameBoard = () => {
+  const [currentTileArrangement, setCurrentTileArrangement] = useState([]);
+
+  const createBoard = () => {
+    const randomTileArrangement = [];
+
+    for (let i = 0; i < Math.pow(width, 2); i++) {
+      const randomTile =
+        gameTiles[Math.floor(Math.random() * gameTiles.length)];
+      randomTileArrangement.push(randomTile);
+    }
+    setCurrentTileArrangement(randomTileArrangement);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  console.log(currentTileArrangement);
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentTileArrangement.map((gameTile, index) => (
+          <img key={index} style={{ backgroundColor: gameTile }}></img>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default GameBoard;
+
+// 23:14
