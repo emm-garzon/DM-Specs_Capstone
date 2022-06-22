9a81703ebae4ad1b0e49747310ff1bdb5876296a
emm-garzon <eggarzon@gmail.com>
2022-06-22 12:57:13 -0700

    created index.js, added necessary dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+const PORT = 8000;
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+app.listen(PORT, () =>
+  console.log(`The server is running successfully on PORT: ${PORT}`)
+);
+
+// 14:29
